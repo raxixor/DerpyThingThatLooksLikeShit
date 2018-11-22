b831559550a16a62126541c147b69455ec55b885
Rax Ixor <raxixor@gmail.com>
2018-11-22 19:59:59 +0200

    run bat & gitign

---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+echo -----------------------------------------------
+echo Remember to change music directory in 'app.py'!
+echo -----------------------------------------------
+"./venv/Scripts/python.exe" app.py
+PAUSE
